bc9c924bbd1f1db9a91c6340214d763b1a6741e5
Mir0730 <sarimquraishi@mail.weber.edu>
2024-12-05 11:24:10 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("team-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Team Created Successfully!");
+});
+document.getElementById("contact-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    alert("Message Sent!");
+});
